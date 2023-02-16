18e5986e507c7a0cab10fa3b4f1f9b449cf2edc1
ahsn.dev <ahsn.dev@gmail.com>
2023-02-16 08:33:38 +0330

    add files

--- question8/main.js
+++
@@ -1,6 +0,0 @@
-const closeButtons = document.querySelectorAll(".close-button");
-closeButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.target.parentElement.remove();
-  });
-});
